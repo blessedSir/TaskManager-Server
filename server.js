ca149840d4e237c317ac3d22072e29d8e14aa412
blessed <qx.dicawl@gmail.com>
2025-11-10 22:21:35 +0200

    full api

---
+++ server.js
@@ -0,0 +1,27 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT || 3001;
+
+// Включаем CORS для всех доменов
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  next();
+});
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
+```
+
+**Опционально - .gitignore:**
+```
+node_modules/
+.env
